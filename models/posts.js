839e92c531fa4fc7cd743e663da505a857fa13a0
huan5678 <huan5678@gmail.com>
2022-04-28 16:19:38 +0800

    Update posts.js

--- models/posts.js
+++ models/posts.js
@@ -1,6 +1,30 @@
 const mongoose = require('mongoose');
 const postSchema = new mongoose.Schema(
   /* 加入欄位驗證 */
+  {
+    name: {
+      type: String,
+      required: [true, '名稱必填'],
+    },
+    tags: {
+      type: ['String'],
+    },
+    type: {
+      type: String,
+    },
+    image: String,
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      select: false,
+    },
+    content: {
+      type: String,
+      required: [true, '貼文內容必填']
+    },
+    likes: Number,
+    comments: Number,
+  }
 );
 
 const Post = mongoose.model('Post', postSchema);
