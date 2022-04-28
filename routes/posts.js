5dc253d9598a6cd17a65befe8a1e20c13772da58
huan5678 <huan5678@gmail.com>
2022-04-28 16:27:01 +0800

    Update posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const Post = require('../models/posts');
-
 router.post('/', async(req, res, next) =>  {
   try {
       /* 請在此填寫答案 */
@@ -9,7 +8,22 @@ router.post('/', async(req, res, next) =>  {
       
       // 驗證是否有 content 欄位 -> 若有則使用 mongoose 語法新增資料 -> 回傳成功回應
       //                       -> 未填寫 content 欄位 -> 回傳失敗回應
-  
+    try {
+      const data = req.body;
+      if(data.content){
+      await Post.create(data);
+      const getAllPosts = await Post.find();
+      res.send({
+        status: true,
+        message: '成功新增一則貼文',
+        posts: getAllPosts,
+      });
+    } catch (err) {
+      res.status(401).send({
+        status: false,
+        message: err.message
+      });
+    }
   } catch (error) {
     res.status(400).json({
         status: 'false',
